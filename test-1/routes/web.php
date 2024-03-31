6958c2c93112216eaab2d1abf360f85f31da62d1
nickshakh <shakhbazyann@gmail.com>
2024-03-31 13:53:08 +0400

    Created first route /products

--- test-1/routes/web.php
+++ test-1/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products',[ProductsController::class,'index'])->name('products.index');
