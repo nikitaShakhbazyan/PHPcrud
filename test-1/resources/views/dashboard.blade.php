5e48922625b947ed47327fc96df2c6f3e7b7cda0
nickshakh <shakhbazyann@gmail.com>
2024-03-31 17:15:27 +0400

    Added a hyperlink

--- test-1/resources/views/dashboard.blade.php
+++ test-1/resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in!") }} <br>
+                    <a href="{{route('products.index')}}" class="text-blue-500 hover:text-blue-950">Products Page</a>
                 </div>
             </div>
         </div>
